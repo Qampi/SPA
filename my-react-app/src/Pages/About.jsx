81ac6fd332ae38972551dc091067bf30581bbe5c
Qampi <asiphe.qampi@gmail.com>
2024-10-12 19:27:05 +0200

    Update About.jsx

--- my-react-app/src/Pages/About.jsx
+++ my-react-app/src/Pages/About.jsx
@@ -11,12 +11,12 @@ function About() {
 
 export default About;*/
 import React from 'react';
-
+import './About.css'; // Import the CSS file
 function About() {
   return (
-    <div class=About.css>
-      <h1>About Us</h1>
-      <p>
+    <div class=About>
+      <h1 class= About h1>About Us</h1>
+      <p class= About p>
         Our mission is to provide a platform for users to support their favorite organizations.
       </p>
     </div>
