04dbd962f837855258c41d86e0dd1f0032385082
Qampi <asiphe.qampi@gmail.com>
2024-10-12 19:59:57 +0200

    Update Home.jsx

--- my-react-app/src/Pages/Home.jsx
+++ my-react-app/src/Pages/Home.jsx
@@ -9,12 +9,27 @@ function Home() {
    );
 }
 export default Home;*/
-import React from 'react';
+/*import React from 'react';
 //import data from './data'; // Make sure the path is correct
 
 function Home() {
   return (
     <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the homepage!</p>
+      {/* You can use data here if needed }
+    </div>
+  );
+}
+
+export default Home;*/
+jsx
+import React from 'react';
+import './Pages.css'; // Import the CSS file
+
+function Home() {
+  return (
+    <div className="Home">
       <h1>Home Page</h1>
       <p>Welcome to the homepage!</p>
       {/* You can use data here if needed */}
